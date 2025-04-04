204873229b6fca330a46acc37cc737fc00da48fb
Balaji <balajivs0305@gmail.com>
2025-04-04 23:10:54 +0530

    updates

--- client/src/app/services/todo.service.ts
+++ client/src/app/services/todo.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+//
 export interface Todo {
   _id?: string;
   title: string;

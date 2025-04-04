e7308a819548034074c270245b7de899f9ee7185
Balaji <balajivs0305@gmail.com>
2025-04-04 22:51:56 +0530

    first-commit

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const todoRoutes = require('./routes/todoRoutes');
+const connectDB = require('./config/db');
+
+require('dotenv').config();
+connectDB();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use('/api', todoRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});

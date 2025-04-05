bf7ad3278dac2ac68a6d071fc7c5b1dfb381f7d2
Balaji <balajivs0305@gmail.com>
2025-04-05 14:00:42 +0530

    updates

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ const PORT = process.env.PORT || 3000;
 app.use(
   cors({
     origin: "https://todo-app-mean.vercel.app",
+    // origin: "http://localhost:4200",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",

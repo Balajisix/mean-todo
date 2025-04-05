c53c2a78ab9c6b9dea07ca227afc1c6bfd06628f
Balaji <balajivs0305@gmail.com>
2025-04-05 21:15:51 +0530

    updated middleware

--- server/controllers/todoController.js
+++ server/controllers/todoController.js
@@ -1,20 +1,23 @@
 const Todo = require('../models/todoModel');
 
 // Create a new todo
-const createTodo = async (req, res) => {
+const createTodo = async(req, res) => {
   try {
-    const todo = new Todo(req.body);
+    const todo = new Todo({
+      ...req.body,
+      user: req.user.userId,
+    });
     const savedTodo = await todo.save();
     res.status(201).json(savedTodo);
-  } catch (err) {
+  } catch(err) {
     res.status(400).json({ error: err.message });
   }
-};
+}
 
 // Get all todos
 const getTodos = async (req, res) => {
   try {
-    const todos = await Todo.find();
+    const todos = await Todo.find({ user: req.user.userId });
     res.status(200).json(todos);
   } catch (err) {
     res.status(500).json({ error: err.message });
@@ -24,7 +27,12 @@ const getTodos = async (req, res) => {
 // Update a todo by id
 const updateTodo = async (req, res) => {
   try {
-    const updatedTodo = await Todo.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    const todo = await Todo.findOne({ _id: req.params.id, user: req.user.userId });
+    if (!todo) {
+      return res.status(404).json({ error: 'Todo not found' });
+    }
+    Object.assign(todo, req.body);
+    const updatedTodo = await todo.save();
     res.status(200).json(updatedTodo);
   } catch (err) {
     res.status(400).json({ error: err.message });
@@ -34,7 +42,10 @@ const updateTodo = async (req, res) => {
 // Delete a todo by id
 const deleteTodo = async (req, res) => {
   try {
-    await Todo.findByIdAndDelete(req.params.id);
+    const deletedTodo = await Todo.findOneAndDelete({ _id: req.params.id, user: req.user.userId });
+    if (!deletedTodo) {
+      return res.status(404).json({ error: 'Todo not found' });
+    }
     res.status(204).end();
   } catch (err) {
     res.status(500).json({ error: err.message });
--- server/models/todoModel.js
+++ server/models/todoModel.js
@@ -3,7 +3,8 @@ const Schema = mongoose.Schema;
 
 const TodoSchema = new Schema({
   title: { type: String, required: true },
-  completed: { type: Boolean, default: false }
+  completed: { type: Boolean, default: false },
+  user: { type: Schema.Types.ObjectId, ref: 'User', required: true }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Todo', TodoSchema);

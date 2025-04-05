0c0558efb6d42384df84144087e987711256c0a2
Balaji <balajivs0305@gmail.com>
2025-04-05 22:33:11 +0530

    updates

--- server/models/todoModel.js
+++ server/models/todoModel.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 const TodoSchema = new Schema({
   title: { type: String, required: true },
   completed: { type: Boolean, default: false },
-  user: { type: Schema.Types.ObjectId, ref: 'User', required: true }
+  user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  deadline: { type: Date },
 }, { timestamps: true });
 
 module.exports = mongoose.model('Todo', TodoSchema);

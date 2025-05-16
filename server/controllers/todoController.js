26d7eb869520fc8df2bb6c49f780a47a051ac7b2
Balaji <balajivs0305@gmail.com>
2025-05-16 19:32:01 +0530

    fixed performance issue

--- server/controllers/todoController.js
+++ server/controllers/todoController.js
@@ -1,20 +1,19 @@
 const Todo = require('../models/todoModel');
 
 // Create a new todo
-const createTodo = async(req, res) => {
+const createTodo = async (req, res) => {
   try {
-    const todo = new Todo({
+    const newTodo = await Todo.create({
       ...req.body,
       user: req.user.userId,
     });
-    const savedTodo = await todo.save();
-    res.status(201).json(savedTodo);
-  } catch(err) {
+    res.status(201).json(newTodo);
+  } catch (err) {
     res.status(400).json({ error: err.message });
   }
-}
+};
 
-// Get all todos
+// Get all todos 
 const getTodos = async (req, res) => {
   try {
     const todos = await Todo.find({ user: req.user.userId })
@@ -28,29 +27,37 @@ const getTodos = async (req, res) => {
   }
 };
 
-
-// Update a todo by id
+// Update a todo by ID 
 const updateTodo = async (req, res) => {
   try {
-    const todo = await Todo.findOne({ _id: req.params.id, user: req.user.userId });
-    if (!todo) {
+    const updatedTodo = await Todo.findOneAndUpdate(
+      { _id: req.params.id, user: req.user.userId },
+      req.body,
+      { new: true, runValidators: true, context: 'query' }
+    ).lean();
+
+    if (!updatedTodo) {
       return res.status(404).json({ error: 'Todo not found' });
     }
-    Object.assign(todo, req.body);
-    const updatedTodo = await todo.save();
+
     res.status(200).json(updatedTodo);
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
 };
 
-// Delete a todo by id
+// Delete a todo by ID
 const deleteTodo = async (req, res) => {
   try {
-    const deletedTodo = await Todo.findOneAndDelete({ _id: req.params.id, user: req.user.userId });
+    const deletedTodo = await Todo.findOneAndDelete({
+      _id: req.params.id,
+      user: req.user.userId,
+    }).lean();
+
     if (!deletedTodo) {
       return res.status(404).json({ error: 'Todo not found' });
     }
+
     res.status(204).end();
   } catch (err) {
     res.status(500).json({ error: err.message });
@@ -61,5 +68,5 @@ module.exports = {
   createTodo,
   getTodos,
   updateTodo,
-  deleteTodo
-}
+  deleteTodo,
+};
